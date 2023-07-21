ec62b394ed75ccc32cddf8cb4d09c4a95ff276d2
pusit-hanp <pusit.hanp@gmail.com>
2023-07-20 23:47:20 -0600

    upload image

--- server.js
+++ server.js
@@ -59,17 +59,66 @@ app.use(
 // Serve static files from the "images" directory
 //app.use('/images', express.static(path.join(__dirname, '../public/images')));
 
-const upload = multer({ dest: 'public/images/raws' });
+const storage = multer.diskStorage({
+  destination: path.resolve('public/images/raws'), // Use path.resolve() to create an absolute path
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(
+      null,
+      file.originalname + '-' + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+/////////////////////////////////////////////////////////////////
 app.post(
-  '/test_upload',
-  upload.single('product_image'),
-  function (req, res, next) {
-    console.log(req.file);
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
-    res.status(200);
+  '/api/image/upload',
+  upload.single('imageFile'),
+  async function (req, res, next) {
+    try {
+      // console.log(req.file); // Check if the file details are logged correctly
+      // console.log(req.body); // Check other form fields
+
+      // Save image details to MongoDB
+      const imageDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        seller: '', // Seller information here
+        likes: 0, // Initialize the likes to 0
+        views: 0, // Initialize the views to 0
+        status: 'Available', // Assuming the default status is 'Available'
+        // imageLocation: req.file.path, // Using absolute path
+        imageLocation: `./images/raws/${req.file.filename}`, // Using relative path
+
+        dateCreated: new Date(),
+        dateEdited: new Date(),
+        tags: req.body.tags.split(','), // Convert tags string to an array
+        price: parseFloat(req.body.price), // Convert price string to a floating-point number
+      };
+
+      // Save the image details to the 'images' collection in MongoDB
+      await db.collection('images').insertOne(imageDetails);
+
+      res.sendStatus(200);
+    } catch (error) {
+      console.error('Error handling image upload:', error.message);
+      res.sendStatus(500);
+    }
   }
 );
+
+// app.post('/test_upload', upload.single('imageFile'), function (req, res, next) {
+//   try {
+//     console.log(req.file); // Check if the file details are logged correctly
+//     console.log(req.body); // Check other form fields
+//     res.sendStatus(200);
+//   } catch (error) {
+//     console.error('Error handling image upload:', error.message);
+//     res.sendStatus(500);
+//   }
+// });
 
 app.post('/api/payment/create-checkout-session', async (req, res) => {
   const { product } = req.body;
