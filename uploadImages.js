ea30b226b1f5e85931e8987224474457ca12ba4c
pusit-hanp <pusit.hanp@gmail.com>
2023-07-30 03:22:23 -0600

    update test data

--- uploadImages.js
+++ uploadImages.js
@@ -1,8 +1,12 @@
+const { ObjectId } = require('mongodb');
+
+var id = new ObjectId('64aa26b58ceb9a34a209fad2');
+
 var imageTemplate = {
   title: 'Image',
   description:
     'Oil and gas are essential natural resources that play a pivotal role in the global energy sector. These hydrocarbons, formed over millions of years from organic matter, are extracted from deep within the Earth is crust through drilling processes. Once extracted, oil and gas are refined into various products such as gasoline, diesel, and petrochemicals, which are integral to transportation, manufacturing, and everyday life. Despite their widespread use, concerns about environmental impacts and climate change have led to a growing focus on renewable energy sources as alternatives. Nevertheless, oil and gas remain crucial components of the energy mix, shaping economies and influencing geopolitics worldwide. Efforts to balance energy needs with sustainability will continue to drive innovation and transition towards cleaner technologies in the oil and gas industry.',
-  seller: 12345678,
+  seller: id,
   likes: 0,
   views: 500,
   status: 'Active',
